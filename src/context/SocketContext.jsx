00f232c3b42f28328d16def4241a20b0f6aa73c8
Vo Thai Bao <thaibaovo2kdev@gmail.com>
2024-04-04 10:28:30 +0700

    update socket context

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5001", {
+			const socket = io("https://chat-app-be-5ucv.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
